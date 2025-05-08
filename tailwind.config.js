a8453c32a5a7d5ef18dbc4d0dbe198fb5f49d565
hemanthj257 <hemanthj257@gmail.com>
2025-05-08 13:53:37 +0530

    tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{astro,html,js,jsx,ts,tsx}'], // Include all relevant file types
+  theme: {
+    extend: {}, // You can customize your theme here
+  },
+  plugins: [], // Add any Tailwind plugins here if needed
+};
+
